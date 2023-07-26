02b82bdc85de705441e9e5da001976f07921b1d7
KVA <vlada.koroleva@inbox.ru>
2023-07-26 03:08:37 +0300

    add checks for input string

--- TestingSystem/src/TestingSystem.java
+++ TestingSystem/src/TestingSystem.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class TestingSystem {
+
     public static void main(String[] args) {
         // объявление переменных
         double KOEF_PASS_TEST = 0.6; // коэффициент успешного прохождения
@@ -12,13 +13,17 @@ public class TestingSystem {
         int numberQuestion = 0; // номер вопроса
         int numberPossibleAnswer = 0; // номер варианта ответа
         int answer; // ответ участника
+        String answerStr; // ответ участника типа String
         int questionCount = 0; // кол-во вопросов
         int possibleAnswerCount = 0; // кол-во вариантов ответов
         int answerRightCount = 0; // количество правильных ответов участника
         int answerWrongCount = 0; // количество неправильных ответов участника
         int answerPass; // кол-во ответов для успешного прохождения
         int koefPassCur; // процент прохождения участника
-        boolean answerCorrect; // флаг корректного ответа участника: true - корректный, false - некорректный
+        boolean notEmtyStr = false; //флаг введенной пустой строки: true - строка не пустая, false - строка пустая
+        boolean getNumber = false; // флаг того, что введено число: true - введено число, false - введено не число
+        boolean answerInPossible = false; // флаг того, что ответ - один из вариантов ответа: true - корректный, false - некорректный
+        boolean answerCorrect = false; // флаг корректного ответа участника: true - корректный, false - некорректный
 
         // инициализация переменных
         questionArr = new String[]{
@@ -35,9 +40,9 @@ public class TestingSystem {
 
         possibleAnswerArr = new String[][]{
                 {
-                    "планетная система со звездой в центре и природными космическими объектами, которые вращаются вокруг Солнца",
+                        "планетная система со звездой в центре и природными космическими объектами, которые вращаются вокруг Солнца",
                         "звездная система с планетами",
-                       "система из Солнца и планет"
+                        "система из Солнца и планет"
                 },
                 {
                         "рукав Ориона",
@@ -45,7 +50,7 @@ public class TestingSystem {
                         "местную группу галактик"
                 },
                 {
-                    "1 млрд лет",
+                        "1 млрд лет",
                         "4,57 млрд лет",
                         "3 млрд лет",
                         "7 млрд лет"
@@ -67,15 +72,50 @@ public class TestingSystem {
                 numberPossibleAnswer = j + 1;
                 System.out.println(numberPossibleAnswer + ". " + possibleAnswerArr[i][j]); // Вывод вариантов ответов
             }
+            answer = 1;
             do {
                 System.out.println("Пожалуйста, введите номер вашего ответа:");
-                answer = consol.nextInt();
-                if (answer > 0 && answer <= possibleAnswerCount){ // как задать множество целых чисел?
-                    answerCorrect = true;
+                answerStr = consol.nextLine();
+
+                //Проверка на пустую строку
+
+                if (answerStr == "") {
+                    notEmtyStr = false;
+                    System.out.println("Ответ некорректен. Вы не дали ответ.");
                 } else {
-                    answerCorrect = false;
-                    System.out.println("Ответ некорректен.");
+                    notEmtyStr = true;
                 }
+
+                //Проверка на буквы
+                //разбиваю стрингу на чары, прохожусь по каждому и смотрю принадлежит ли он от 0 до 9
+                getNumber = true;
+                for (int j = 0; j < answerStr.length() && getNumber; j++) {
+                    getNumber = false;
+                    if (j == 0){
+                        if (49<=(int)answerStr.toCharArray()[j] && (int)answerStr.toCharArray()[j] <= 57) { //Проверка,что первая цифра - не ноль
+                            getNumber = true;
+                        }
+                    } else {
+                        if (48 <= (int) answerStr.toCharArray()[j] && (int) answerStr.toCharArray()[j] <= 57) { //аля множество целых чисел в чаровском представлении '0'-k=48,'9'-k=57
+                            getNumber = true;
+                        }
+                    }
+                }
+                if (!getNumber) {
+                    System.out.println("Ответ некорректен. Ответом должно являться число.");
+                }
+
+                //Проверка на то, что ответ - один из вариантов ответа
+                if (getNumber&&notEmtyStr) {
+                    answer = Integer.parseInt (answerStr);
+                    if (answer > 0 && answer <= possibleAnswerCount) {
+                        answerInPossible = true;
+                    } else {
+                        answerInPossible = false;
+                        System.out.println("Ответ некорректен. Ваш ответ не является одним из вариантов ответа.");
+                    }
+                }
+                answerCorrect = notEmtyStr && getNumber && answerInPossible;
             } while (!answerCorrect);
 
             //если ответ дан правильный, то прибавляем счетчик правильных ответов
